53a58295e15c9e970ce125a2c01f5e2e7cbd124c
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-06-28 13:47:03 +0700

    [WIP] work in progress user input

--- App.go
+++ App.go
@@ -2,17 +2,53 @@ package main
 
 import (
 	"GoTempConverter/src/temp"
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 )
 
 func main() {
 	userInput()
 }
+func menu() {
+	fmt.Println("a) celsius -> fahrenheit")
+	fmt.Println("b) fahrenheit -> kelvin")
+	fmt.Println("c) kelvin -> rankine")
+	fmt.Println("d) rankine -> reaumur")
+}
 
 func userInput() {
-	var c temp.Celsius
-	c.Temperature = 20
-	c.Category = "cold"
+	menu()
+	fmt.Println("choice : ")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	input := scanner.Text()
 
-	fmt.Println(c)
+	switch input {
+	case "a":
+		fmt.Println("insert the celsius temperature (int)")
+		scanner.Scan()
+		temperatures, _ := strconv.Atoi(scanner.Text())
+
+		fmt.Println("insert the category ")
+		scanner.Scan()
+		categories := scanner.Text()
+		createC(temperatures, categories)
+	}
+}
+
+func createC(temperature int, category string) {
+	var c temp.Celsius
+	m := make(map[string]int)
+
+	c.Temperature = temperature
+	c.Category = category
+
+	m[c.Category] = c.Temperature
+
+	for k, v := range m {
+		fmt.Printf("category : %s temperature : %d", k, v)
+	}
+
 }
